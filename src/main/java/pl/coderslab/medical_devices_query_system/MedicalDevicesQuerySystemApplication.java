5803719b308f38660958ec1e4ea58ff3ab4bc369
Ewa Sieminska <agnieszka.sieminska@gmail.com>
2020-09-09 20:12:23 +0200

    pliki konfiguracyjne

---
+++ src/main/java/pl/coderslab/medical_devices_query_system/MedicalDevicesQuerySystemApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.medical_devices_query_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDevicesQuerySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalDevicesQuerySystemApplication.class, args);
+    }
+
+}
