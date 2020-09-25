3583219d531d33018ba5e61cc478a661db8dbb68
Ewa Sieminska <agnieszka.sieminska@gmail.com>
2020-09-25 10:49:41 +0200

    plik do skryptu sql

---
+++ src/main/resources/static/sql/insert.sql
@@ -0,0 +1,8 @@
+/*INSERT INTO `hospitals` (`active`, `created_on`, `city`, `name`) VALUES
+('Plan Jarski', 'Opis planu 1', '2018-10-17 14:27:05', 1),
+(null, 'Plan Mięsny', 'Opis planu 2', '2018-10-17 14:27:05', 1),
+(null, 'Plan Śniadaniowy', 'Opis planu 3', '2018-10-17 14:27:05', 1),
+(null, 'Plan Redukcja', 'Opis planu 4', '2018-10-17 14:27:05', 1),
+(null, 'Plan Dużo białka', 'Opis planu 5', '2018-10-17 14:27:05', 1),
+(null, 'Kapuściana dieta', 'Opis planu 6', '2018-10-17 14:27:05', 1)
+;*/
